f1a08d0b48fe9f49d318522a84cb3149b3dd8caa
Evgenia <evgeniayurkova@gmail.com>
2023-12-20 12:31:41 +0200

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
 import SoundSection from "./components/SoundSection";
 import WebgiViewer from "./components/WebgiViewer";
+import Loader from './components/Loader';
 
 function App() {
   const webgiViewerRef = useRef();
@@ -15,11 +16,12 @@ function App() {
 
   return (
     <div className="App">
+      <Loader />
       <div ref={contentRef} id="content">
         <Nav />
         <Jumbotron />
         <SoundSection />
-        <DisplaySection triggerPreview={handlePreview} />        
+        <DisplaySection triggerPreview={handlePreview} />
       </div>
       <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
